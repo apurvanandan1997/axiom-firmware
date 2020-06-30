8af2ded96d51236f05c972b260e2f6324fc460a9
preetimenghwani <61456975+preetimenghwani@users.noreply.github.com>
2020-06-30 23:58:12 +0530

    Update ser_to_par.vhd

--- peripherals/soc_main/ser_to_par.vhd
+++ peripherals/soc_main/ser_to_par.vhd
@@ -56,13 +56,13 @@ entity ser_to_par is
         serdes_phase  : in std_logic;
         serdes_rst    : in std_logic;
         --
-        ser_data : in std_logic_vector (CHANNELS - 1 downto 0);
+        ser_data : in std_logic_vector(CHANNELS - 1 downto 0);
         --
         par_clk    : in  std_logic;
         par_enable : out std_logic;
-        par_data   : out par12_a (CHANNELS - 1 downto 0);
+        par_data   : out par12_a(CHANNELS - 1 downto 0);
         --
-        bitslip       : in  std_logic_vector (CHANNELS - 1 downto 0);
+        bitslip       : in  std_logic_vector(CHANNELS - 1 downto 0);
         count_enable  : in  std_logic
     );
 
@@ -72,26 +72,29 @@ end entity ser_to_par;
 architecture RTL of ser_to_par is
 
     attribute KEEP_HIERARCHY of RTL : architecture is "TRUE";
-  
+
     ------------------------------------------------------------------------------
     -- data_dval_low, data_lval_low and data_fval_low are used to set the data 
     -- value to a constant testpattern i.e representing TP1, TP2 as in CMV sensor
     ------------------------------------------------------------------------------
-    constant data_dval_low  : std_logic_vector(11 downto 0) := x"FFF";
-    constant data_lval_low  : std_logic_vector(11 downto 0) := x"EEE";
-    constant data_fval_low  : std_logic_vector(11 downto 0) := x"DDD";
-  
+    constant data_dval_low : std_logic_vector(11 downto 0) := x"FFF";
+    constant data_lval_low : std_logic_vector(11 downto 0) := x"EEE";
+    constant data_fval_low : std_logic_vector(11 downto 0) := x"DDD";
+
     signal par_data_buf : par12_a(CHANNELS - 1 downto 0);
-    signal lval_count   : std_logic_vector(1 downto 0)  := (others => '0'); 
-    signal ctrl_in  : std_logic_vector(11 downto 0) := (others => '0');
-    signal counter  : std_logic_vector(11 downto 0) := data_dval_low;
+    signal lval_count   : std_logic_vector(1 downto 0) := (others => '0');
+    signal ctrl_in : std_logic_vector(11 downto 0) := (others => '0');
+    signal counter : std_logic_vector(11 downto 0) := data_dval_low;
 
 begin
 
+    ------------------------------------------------------------------------------
+    -- counter_proc : FSM to handle the timing of control signals
+    ------------------------------------------------------------------------------
     counter_proc : process(serdes_clkdiv)
     begin
         if rising_edge(serdes_clkdiv) then
-            if count_enable ='1' and serdes_phase = '1' then
+            if count_enable = '1' and serdes_phase = '1' then
                 if counter = x"07F" then
                     counter <= data_dval_low;
                     ctrl_in <= x"007";
@@ -123,18 +126,21 @@ begin
     end process;
 
     ------------------------------------------------------------------------------
-    -- Assigning fake data (counter) to par_data_buf 
+    -- gen_pat0 : Assigning fake data and channel info to par_data_buf 
     ------------------------------------------------------------------------------
-    GEN_PAT0 : for I in (CHANNELS - 2) downto 0 generate
-        par_data_buf(I)(11 downto 8) <=  std_logic_vector(to_unsigned(I/2,4));                
-        par_data_buf(I)(7 downto 0)  <=  counter(7 downto 0);
+    gen_pat0 : for I in (CHANNELS - 2) downto 0 generate
+        par_data_buf(I)(11 downto 8) <= std_logic_vector(to_unsigned(I / 2, 4));
+        par_data_buf(I)(7 downto 0)  <= counter(7 downto 0);
     
     end generate;
 
     par_data <= par_data_buf;
-    par_data_buf(CHANNELS-1) <= ctrl_in;
+    par_data_buf(CHANNELS - 1) <= ctrl_in;
 
-    push_proc : process (par_clk)
+    ------------------------------------------------------------------------------
+    -- push_proc : Generates par_enable signal to demarcate valid data
+    ------------------------------------------------------------------------------
+    push_proc : process(par_clk)
         variable phase_d_v : std_logic;
 
     begin
